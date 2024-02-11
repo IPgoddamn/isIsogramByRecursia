cb8efd72a6b1c6e8c98f39cd9f643a9b910dced6
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2024-02-11 22:11:26 +0200

    make a base operation for working with filed

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,14 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         String s = "abcdD";//System.out.println(duplicateCount(s));
-        System.out.println(isIsogram(s));
-
+        whoLikesIt("Jacob", "Alex");
     }
-    public static boolean  isIsogram(String str) {
-        if(str.isEmpty()) return true;
-        String s = str.toLowerCase();
-        String uniqueSymbol = s.substring(0, 1);
-        String test = s.substring(1);
-        if(test.contains(uniqueSymbol))return false;
-        return isIsogram(test);
+    public static String whoLikesIt(String... names) {
+        if(names.length == 0) return "no one likes this";
+        if(names.length == 1) return names[0] + " likes this";
+        if(names.length == 2) return names[0] + " and " + names[1] + " like this";
+        if(names.length == 3) return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        if(names.length > 3) return names[0] + ", " + names[1] + " and " + (names.length - 2) + " like this";
+        return "";
     }
 }
